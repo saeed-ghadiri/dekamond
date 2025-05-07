044c717039789239c25f011b76959710b29c7dec
saeed-ghadiri <saeidghadiry.gh@gmail.com>
2025-05-08 00:47:54 +0330

    update for build issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // or your subdirectory path if needed
+  build: {
+    outDir: 'dist', // Explicitly set (though this is default)
+  }
 })
