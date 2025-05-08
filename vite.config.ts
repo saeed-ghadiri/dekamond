2eff98e36ab14c877a5f55c32aacb85fdf460ade
saeed-ghadiri <saeidghadiry.gh@gmail.com>
2025-05-08 09:19:44 +0330

    undo last commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/', // or your subdirectory path if needed
-  build: {
-    outDir: 'dist', // Explicitly set (though this is default)
-  }
 })
