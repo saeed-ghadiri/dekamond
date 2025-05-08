9e17b5db9aba6ed39b104a5fc31639dc24419c5c
saeed-ghadiri <saeidghadiry.gh@gmail.com>
2025-05-08 17:44:37 +0330

    fix about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,12 +6,9 @@ import about3 from "../assets/images/about03.jpg";
 const About = () => {
   return (
     <div className="">
-      <h2 className="text-2xl font-bold mb-4">About Us</h2>
-      <div className="flex flex-col gap-4">
-        <img src={about1} alt="About section 1" className="w-full rounded" />
+        <img src={about1} alt="About section 1" className="w-full rounded-lg" />
         <img src={about2} alt="About section 2" className="w-full rounded-lg" />
         <img src={about3} alt="About section 3" className="w-full rounded-lg" />
-      </div>
     </div>
   );
 };
